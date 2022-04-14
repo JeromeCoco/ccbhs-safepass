520eefad2e88c2e6ef6df17e7a80180a1932fe9c
Jerome Coco <jerome.coco@august99.com>
2022-04-14 23:09:14 +0800

    add ajax

--- main.js
+++ main.js
@@ -7,8 +7,25 @@ var html5QrcodeScanner = new Html5QrcodeScanner(
 );
         
 function onScanSuccess(decodedText, decodedResult) {
-    // Handle on success condition with the decoded text or result.
     alert(`Scan result: ${decodedText}`, decodedResult);
+
+    var array_scanned = decodedResult.split(","),
+        name = array_scanned[0],
+        grade = array_scanned[1],
+        section = array_scanned[2];
+
+    $.ajax({
+        type : 'get',
+        url : 'https://regardable-passivat.000webhostapp.com/logbook.php', 
+        data: {name, grade, section}, 
+        success:function(data) {     
+            alert(data);
+        },
+        error: function(xhr, status, error) {
+            var err = eval("(" + xhr.responseText + ")");
+            alert(err.Message);
+        }
+    });
 }
 
 function onScanError(errorMessage) {
